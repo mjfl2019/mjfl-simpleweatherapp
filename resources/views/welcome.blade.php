1b1896f0343d469ee08e8dc789d579c7cb0e3b83
mjlusoc <marcjericolusoc@gmail.com>
2022-11-22 01:57:57 +0800

    Add files via upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,3 @@
-<?php
-
-
-
-
-?>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -12,8 +6,13 @@
     <link rel="stylesheet" href="css/main.css">
     <script src="js/jquery-3.6.1.min.js"></script>
     <script>
-    vCity = "";
+    var vCity = "";
+    var vWeather = "";
+    
     $(function() {
+
+        $("#tblWeatherStatus").hide();
+        $("#imgWeather").hide();
 
         checkInternetConnection();
         execOpenWeatherMapAPICall();
@@ -49,18 +48,59 @@
     }
     
     function execOpenWeatherMapAPICall() {
-        
+        var vSrc = "";
         $.ajax({
 
                 type: 'POST',
                 url: 'https://api.openweathermap.org/data/2.5/weather?q='+vCity+',japan&appid=49d712725a899af85dd5b4b45c724a57',
-                /*ata: { queue_id:1},
-                cache: false,*/
                 tryCount : 0,
                 retryLimit : 3,
                 success: function (d) {
                     var retData = JSON.stringify(d);
+                    var vJSONparsed = JSON.parse(retData).weather;
+                    var vMain = vJSONparsed[0].main;
                     console.log(retData);
+                    console.log(vMain);
+                    console.log(vJSONparsed[0].description);
+                    $("#tblWeatherStatus").show();
+                    $("#imgWeather").show();
+                    $("#pWeather").text(vMain);
+                    $("#pDesc").text(vJSONparsed[0].description);
+
+                    if (vMain == "Clouds") {
+                        vSrc = "/images/cloudy.gif";
+                    } else if (vMain == "Rain") {
+                        vSrc = "/images/rainy.gif";
+                    } else if (vMain == "Sun") {
+                        vSrc = "/images/sunny.gif";
+                    } else if (vMain == "Clear") {
+                        vSrc = "/images/clear.gif";
+                    } else if (vMain == "Fog") {
+                        vSrc = "/images/foggy.gif";
+                    } else if (vMain == "Mist") {
+                        vSrc = "/images/misty.gif";
+                    } else if (vMain == "Wind") {
+                        vSrc = "/images/windy.gif";
+                    } else if (vMain == "Drizzle") {
+                        vSrc = "/images/rainy.gif";
+                    } else if (vMain == "Thunderstorm") {
+                        vSrc = "/images/thunderstorm.gif";
+                    } else if (vMain == "Squall") {
+                        vSrc = "/images/windy.gif";
+                    } else if (vMain == "Haze") {
+                        vSrc = "/images/hazy.gif";
+                    } else if (vMain == "Tornado") {
+                        vSrc = "/images/tornado.gif";
+                    } else if (vMain == "Ash") {
+                        vSrc = "/images/volcanicash.gif";
+                    } else if (vMain == "Smoke") {
+                        vSrc = "/images/hazy.gif";
+                    } else if (vMain == "Dust") {
+                        vSrc = "/images/dusty.gif";
+                    } else if (vMain == "Snow") {
+                        vSrc = "/images/snowy.gif";
+                    }
+                    $("#imgWeather").attr('src',vSrc);
                 },
                 error: function (jqXHR, exception) {
                         var msg = '';
@@ -95,6 +135,7 @@
     }
 
     function execFourSquarePlacesAPICall() {
+        var vTextPlaces = "";
         $.ajax({
                 type: 'GET',
                 url: 'https://api.foursquare.com/v3/places/search?near='+vCity+',JP',
@@ -103,7 +144,18 @@
                 retryLimit : 3,
                 success: function (d) {
                     var retData = JSON.stringify(d);
-                    console.log(retData);
+                    var vJSONparsed = JSON.parse(retData).results;
+                    for (var i = 0; i < JSON.parse(retData).results.length; i++) {
+                        var vName = "<tr><td><p style='color:darkcyan;'>-NAME- </p><strong>"+vJSONparsed[i].name+"</strong></td>";
+                        var vAddress = "<td><p style='color:darkcyan;'>-ADDRESS- </p>"+"<strong>"+vJSONparsed[i].location['formatted_address']+"</strong></td></tr>";
+                        console.log(vName);
+                        console.log(vAddress);
+                        vTextPlaces = vTextPlaces + vName + vAddress;
+                    }
+                    $("#tblPlaces").html(vTextPlaces);
+                    //console.log(vJSONparsed);
+                    //console.log(vJSONparsed[2]['categories']);
+                    //console.log(vJSONparsed[0]['categories'][1]['name']);
                 },
                 error: function (jqXHR, exception) {
                         var msg = '';
@@ -140,8 +192,8 @@
 </head>
 <body>
     <div align="left"><h1>HELLO JAPAN!</h1></div>
-    <div align="left"><h3>Hey! Let's check the weather before we go!<h3></div>
-    <div align="left"><h3>Select a City to Check<h3></div>
+    <div align="left"><h3>Hey! Let's check the weather before you go!<h3></div>
+    <div><h3>Select a City to Check<h3></div>
     <select name="listCities" id="listCities">
         <option value="tokyo">TOKYO</option>
         <option value="nagoya">NAGOYA</option>
@@ -188,17 +240,33 @@
         <option value="fukuoka">FUKUOKA</option>
         <option value="saga">SAGA</option>
         <option value="kumamoto">KUMAMOTO</option>
-        <option value="kyushu">KYUSHU</option>
         <option value="miyazaki">MIYAZAKI</option>
         <option value="satsumasendai">SATSUMASENDAI</option>
         <option value="nagasaki">NAGASAKI</option>
         <option value="kagoshima">KAGOSHIMA</option>
     </select>
     <br>
-    <div align="left"><h3>OR<h3></div>
-    <div align="left"><h3>Type the City you know in Japan before clicking enter button below.<h3></div>
-    <div align="left"><input type="text" name="txtCity" id="txtCity" placeholder="Enter a City here"/></div>
-    <br>
-    <div align="left"><button name="btnEnter" id="btnEnter">ENTER</button></div>
+    <div align="left"><h2>or<h2></div>
+    <div align="left"><h3>Type the City you know in Japan then click the search button.<h3></div>
+    <div align="left"><input type="text" name="txtCity" id="txtCity" placeholder="Type a city here."/>&nbsp;<button name="btnEnter" id="btnEnter">SEARCH</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img id="imgWeather" width="200" height="120" style="border-radius:7%;"></div>
+    <table width="50%" id="tblWeatherStatus">
+        <tr width="auto">
+            <td width="25%" align="right">
+                <h4>Weather Status:</h4>
+            </td>
+            <td width="25%" align="center">
+                <i><h3 style="color:blue;" id="pWeather"></h3></i>
+            </td>
+            <td width="25%" align="right">
+                <h4>Description:</h4>
+            </td>
+            <td width="25%" align="center">
+                <i><h3 style="color:blue;" id="pDesc"></h3></i>
+            </td>
+        </tr>
+    </table>
+    <h3 style="color:#ff0000;">NEARBY PLACES (Popular Restaurants, Museum, etc.)</h3>
+    <table width="50%" id="tblPlaces">
+    </table>
 </body>
 </html>
